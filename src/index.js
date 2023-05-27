16b11f436d41faad3ee881718c62e5d9bba2edc8
NjiDomi <133626868+NjiDomi@users.noreply.github.com>
2023-05-27 08:10:27 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-) 
-// this line indicates that all tghe contents of this file should be rendered in the the root id of thee html file
+    <App />
+  </React.StrictMode>
+);
+
