4e0a4c839db34c4dff2489437630b4d737ee0d4c
NjiDomi <133626868+NjiDomi@users.noreply.github.com>
2023-06-11 12:48:21 +0100

    contact page updated

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,95 +1,135 @@
-import React from 'react'
-import './contact.css'
-import {MdOutlineEmail} from 'react-icons/md'
-import {AiOutlineLinkedin} from 'react-icons/ai'
-import {BsWhatsapp} from 'react-icons/bs'
-import {useRef} from 'react'
-import emailjs from 'emailjs-com'
+import { React, useState } from "react";
+import { Footer } from "../footer/Footer";
+import "./contact.css";
+import axios from "axios";
+import { Modal } from "../modal/Modal";
+export const Contact = () => {
+  const [openModal, setOpenModal] = useState(false);
 
-const Contact = () => {
-  const form = useRef();
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-  const sendEmail = (e) => {
-      e.preventDefault();
+  function openModalBtnn() {
+    setOpenModal(true);
+  }
 
-      emailjs.sendForm('service_5vuauza', 'template_fdnnl56', form.current, 'EmSdUNQzQuxkUqGWx')
-      e.target.reset() 
-      .then((result) => {
-        console.log(result.text)
-      },(error) =>{
-        console.log(error.text);
+  const submit = async (e) => {
+    e.preventDefault();
+    setMessage("");
+    setName("");
+    setEmail("");
+
+    try {
+      await axios.post("http://localhost:5000/", {
+        name,
+        email,
+        message,
       });
+    } catch (e) {
+      console.log(e);
+    }
+
+    openModalBtnn();
   };
-    return (
-      <section id="contact">
-        <h5>Get in touch</h5>
-        <h2>Contact me</h2>
 
-        <div className="container contact__container">
-          <div className="contact__options">
-            <article className="contact__option">
-              <MdOutlineEmail className="contact__option__icon" />
-              <h4>Email</h4>
-              <h5>myEmail@gmail.com</h5>
-              <a
-                href="mailto:aldenovpoutine99@gmail.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                send a message
-              </a>
-            </article>
+  return (
+    <section className="contact">
+      <div className="content">
+        <h2>Contac Me</h2>
+        <p>
+          <i>
+            I am not always available but I always try my best to be available once in a while <br /> so feel free to leave your messages, I will reply to you whenever I am chanced. <br /> Do not forget am here for you <b>to serve you</b>
 
-            <article className="contact__option">
-              <AiOutlineLinkedin className="contact__option__icon" />
-              <h4>LinkedIn</h4>
-              <h5>Profile</h5>
-              <a
-                href="https://www.linkedin.com/in/brown-djomo-844b96164/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Tape me on LinkedIn
-              </a>
-            </article>
-
-            <article className="contact__option">
-              <BsWhatsapp className="contact__option__icon" />
-              <h4>Whatsapp</h4>
-              <h5>Direct message</h5>
-              <a
-                href="https://web.whatsapp.com/send?phone=+237657268549"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Whatsapp me
-              </a>
-            </article>
+          </i>
+        </p>
+      </div>
+      <div className="container">
+        <div className="contactinfo">
+          <div className="box">
+            <div className="icon">
+              <i class="fa-solid fa-location-dot"></i>
+            </div>
+            <div className="text">
+              <h2>Address</h2>
+              <p>Limbe,Cameroon</p>
+            </div>
           </div>
 
-          <form ref={form} onSubmit={sendEmail}>
-            <input type="text" name="name" placeholder="full name" required />
-            <input
-              type="email"
-              name="email"
-              placeholder="your email"
-              required
-            />
-            <textarea
-              name="message"
-              id="message"
-              cols="30"
-              rows="10"
-              placeholder="your message"
-              required
-            ></textarea>
-            <button type="submit" className="btn btn-primary">
-              Testified
-            </button>
+          <div className="box">
+            <div className="icon">
+              <i class="fa-solid fa-phone"></i>
+            </div>
+            <div className="text">
+              <h2>Telephone</h2>
+              <p>+237679160921</p>
+            </div>
+          </div>
+
+          <div className="box">
+            <div className="icon">
+              <i class="fa-solid fa-envelope"></i>
+            </div>
+            <div className="text">
+              <h2>Email Address</h2>
+              <p>dominji@gmail.com</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="contactform">
+          <form action="POST">
+            <h2>Send Message</h2>
+            <div className="inputBox">
+              <input
+                type="text"
+                name=""
+                required="required"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+              <span>Full Name</span>
+            </div>
+            <div className="inputBox">
+              <input
+                type="email"
+                name=""
+                required="required"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              <span>Email</span>
+            </div>
+            <div className="inputBox">
+              <textarea
+                name=""
+                required="required"
+                value={message}
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                }}
+              ></textarea>
+              <span>Type your message...</span>
+            </div>
+            <div className="inputBox">
+              <input
+                type="submit"
+                onClick={submit}
+                value="Send"
+                className="openModalBtn"
+              />
+            </div>
           </form>
         </div>
-      </section>
-    );
-}
 
-export default Contact
+        {openModal && <Modal closeModal={setOpenModal} />}
+      </div>
+
+      <Footer />
+    </section>
+  );
+};
