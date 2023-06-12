751cdc9ab2c27f2064ff9790463cdc77ca57af46
NjiDomi <133626868+NjiDomi@users.noreply.github.com>
2023-06-12 04:56:01 +0100

    am here to serve you

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import './services.css'
-import ListServices from './listServices'
-
+import { Footer } from '../footer/Footer'
 
 export const Services = () => {
   return (
-    <section id="services">
-      <h5>What i offer</h5>
-      <h2>Services</h2>
-      <div className="container services__container">
-        <ListServices />
+    <div>
+      <div>
+        <p style={{fontSize:24}} >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nisi enim, libero rem nam minus, quaerat ab debitis sapiente architecto rerum iste eligendi. Iure exercitationem ipsa amet sint placeat ad ullam aspernatur molestiae sed, itaque beatae quo nemo. Quidem expedita quas minus cupiditate maiores vero accusantium repudiandae at culpa molestiae libero voluptatum velit dignissimos sed veniam, excepturi pariatur molestias sint. Eaque, quos vel doloribus officiis sed cumque sequi labore et consequatur fuga, blanditiis, est ab? Laudantium inventore temporibus libero dignissimos accusamus deserunt? Aliquam nemo recusandae ducimus accusamus deleniti veritatis in quia neque fugit necessitatibus atque harum asperiores non eligendi voluptates quisquam officiis fuga pariatur, beatae culpa! Voluptatum autem labore, perferendis quos voluptate sunt repellendus neque nisi provident pariatur repellat dolore quaerat consectetur ut molestias, deserunt fugiat. Quas dolore rerum ad sit asperiores fuga. Explicabo debitis minima sed odit fugiat officiis aliquam! Earum ad, impedit pariatur voluptas soluta, ipsam dignissimos voluptates, est eligendi voluptatem hic minima molestiae reiciendis ea tenetur aliquam quibusdam ut in placeat. Corporis, fugiat. Assumenda dolorum porro facilis velit voluptate sint. Perferendis, aut! Autem incidunt perspiciatis nesciunt molestias odio totam inventore quae? Rem totam labore omnis sit repellendus, nemo tempora cumque. Explicabo obcaecati ducimus at nobis asperiores eaque?
+        </p>
       </div>
-    </section>
-  );
+      <Footer />
+    </div>
+  )
 }
-export default Services;
