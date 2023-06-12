93a4945b5a7d66964e0fb174ca0024ae611596d8
NjiDomi <133626868+NjiDomi@users.noreply.github.com>
2023-06-12 04:41:12 +0100

    updated experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,16 +1,32 @@
-import React from "react";
-import "./experience.css";
-import ListExperiences from "./components/listExperiences";
+import React from 'react'
+import './experience.css'
+import { Footer } from '../footer/Footer'
 
 export const Experience = () => {
   return (
-    <section id="experience">
-      <h5>What skills i have</h5>
-      <h2>My Experience</h2>
-      <div className="container experience__container">
-        <ListExperiences />
-      </div>
-    </section>
+    <div className="experience">
+      <h2>Welcome to my Experience page</h2>
+      <p>
+        I am Nji Dominion a Telecom engineer with one year of experience in
+        Telecommunication. Over the years, I have had the opportunity to work on
+        a variety of projects, from which have allowed me to develop my skills
+        and expertise in telecommunications.<br></br>I have a passion for TECH,
+        and I am committed to delivering high-quality work that meets the needs
+        of my clients.<br/>Throughout my career, I have honed my skills in
+        <hr />
+        <ul>
+          <li>MySQL</li>
+          <li>Huawei</li>
+          <li>Cisco Packet tracer </li>
+        </ul>
+        {/* <br /> */}
+        and I have become an expert in Network configuration<br></br>. Each
+        project is unique, and I take great pride in delivering work that is
+        tailored to the specific needs of my clients. Thank you for visiting my
+        experience page, and I look forward to hearing from you soon
+      </p>
+
+      <Footer />
+    </div>
   );
-};
-export default Experience;
+}
