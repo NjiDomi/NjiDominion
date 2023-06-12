64e98e36c0f498e3d984737910db1ea923200c19
NjiDomi <133626868+NjiDomi@users.noreply.github.com>
2023-06-12 04:58:50 +0100

    updating the whole app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
-import Header from './components/header/Header'
-import Navbar from './components/navbar/Navbar'
-import About from './components/about/About' 
-import Experience from './components/experience/Experience'
-import Services from './components/services/Services'
-import Portfolio from './components/portfolio/Portfolio'
-import Testimonial from './components/testimonial/Testimonial'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
-
+import { About } from './components/about/About';
+import { Contact } from './components/contact/Contact';
+import { Experience } from './components/experience/Experience';
+import { Home } from './components/home/Home';
+import { Navbar } from './components/navbar/Navbar';
+import { Portfolio } from './components/portfolio/Portfolio';
+import { Services } from './components/services/Services';
+import { Testimonial } from './components/Testimonial/Testimonial';
 function App() {
   return (
     <>
-      <Header/>
-      <Navbar/>
-      <About/>
-      <Experience/>
-      <Services/>
-      <Portfolio/>
-      <Testimonial/>
-      <Contact/>
-      <Footer/>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='about' element={<About />}></Route>
+        <Route path='experience' element={<Experience />}></Route>
+        <Route path='services' element={<Services />}></Route>
+        <Route path='portfolio' element={<Portfolio />}></Route>
+        <Route path='testimonial' element={<Testimonial />}></Route>
+        <Route path='contact' element={<Contact />}></Route>
+      </Routes>
     </>
   );
 }
